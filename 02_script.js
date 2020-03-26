31c160c9f818ea667cb2f510c3253f410d79c0d2
charleskarel <charlesawom03@gmail.com>
2020-03-26 23:51:31 +0100

    js

---
+++ 02_script.js
@@ -0,0 +1,13 @@
+let name = 'Awom Charles Chizaram';
+console.log("My name is:" + name)
+let selectedCourses = ('html', 'css', 'javascript')
+console. log (selectedCourses);
+let userName = 'charleskarel';
+console.log(userName);
+const data = [];
+for(let i=0; i<=200; i=+) {
+  if(i%2===1){
+    data.push(i);
+  }
+}
+console.log(data);
